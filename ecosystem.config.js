19048152120bc7d21420bc18433d4e53bb95b816
AndersonKouadioDev <andersonkouadio0118@gmail.com>
2025-06-09 20:38:38 +0000

    mise a jour pm2 test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   apps: [{
-    name: 'erp-website',
+    name: 'erp-test-website',
     script: 'pnpm',
-    args: 'start --port 3003',
-    cwd: '/var/www/turbo/prod/turbo-delivery-erp',
+    args: 'start --port 3013',
+    cwd: '/var/www/turbo/test/turbo-delivery-erp',
     env: {
-      NODE_ENV: 'production',
-      PORT: 3003 // Port sur lequel l'application va tourner
+      NODE_ENV: 'development',
+      PORT: 3013 // Port sur lequel l'application va tourner
     }
   }]
 }
